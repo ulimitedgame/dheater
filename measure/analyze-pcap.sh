aeb640ca399e4e5634d52806305ec8bbb8859410
Szilárd Pfeiffer <szilard.pfeiffer@balasys.hu>
2022-05-13 11:22:36 +0200

    feat(analyze-pcap)!: Implement analysis of IKEv2 handshakes

--- measure/analyze-pcap.sh
+++ measure/analyze-pcap.sh
@@ -71,6 +71,22 @@ ssh*)
     echo -e -n "KEX/GEX reply:\t     "
     tshark -d tcp.port==1-65535,ssh -r $PCAP_FILE_PATH -Y 'ssh.message_code==31 or ssh.message_code==33' 2>/dev/null | wc -l
     ;;
+ikev2*)
+    SERVER_ADDRESS=`tshark -d udp.port==1-65535,isakmp -2 -R 'isakmp.nextpayload==33' -Tfields -e ip.dst -r $PCAP_FILE_PATH | head -1`
+    SERVER_PORT=`tshark -d udp.port==1-65535,isakmp -2 -R 'isakmp.nextpayload==33' -Tfields -e udp.dstport -r $PCAP_FILE_PATH | head -1`
+
+    echo -e "Server:\t\t     ${PROTOCOL}://${SERVER_ADDRESS}:${SERVER_PORT}"
+
+    IN_DATA=`tshark -qz io,stat,0 -2 -R "ip.dst==$SERVER_ADDRESS and udp.dstport==$SERVER_PORT" -r $PCAP_FILE_PATH | tail -2 | head -1 | cut -d '|' -f 4`
+    echo -e "Incoming data:     $IN_DATA bytes"
+    OUT_DATA=`tshark -qz io,stat,0 -2 -R "ip.src==$SERVER_ADDRESS and udp.srcport==$SERVER_PORT" -r $PCAP_FILE_PATH | tail -2 | head -1 | cut -d '|' -f 4`
+    echo -e "Outgoing data:    $OUT_DATA bytes"
+
+    echo -e -n "IKE SA init:\t     "
+    tshark -d udp.port==1-65535,isakmp -r $PCAP_FILE_PATH -Y 'isakmp.flags & 0x08 and isakmp.nextpayload==33' 2>/dev/null | wc -l
+    echo -e -n "IKE SA init:\t     "
+    tshark -d udp.port==1-65535,isakmp -r $PCAP_FILE_PATH -Y 'isakmp.flags & 0x20 and isakmp.nextpayload==33' 2>/dev/null | wc -l
+    ;;
 *)
     echo "Unknown protocol '$PROTOCOL'!"
     exit 1
